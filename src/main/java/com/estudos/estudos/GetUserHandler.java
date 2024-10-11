066bb1dbcd76643db5853d18a1437371a0370ab8
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-10-11 19:17:56 -0400

    feat: Add Gson dependency to pom and implement lambda logic

--- src/main/java/com/estudos/estudos/GetUserHandler.java
+++ src/main/java/com/estudos/estudos/GetUserHandler.java
@@ -4,11 +4,27 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
+
+import java.util.Map;
 
 public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent o, Context context) {
-        return null;
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+
+
+        Map<String, String> pathParameters = input.getPathParameters();
+        String userId = pathParameters.get("userId");
+
+        JsonObject returnValue = new JsonObject();
+        returnValue.addProperty("firstName", "Marcos");
+        returnValue.addProperty("lastName", "Alex");
+        returnValue.addProperty("id", userId);
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.withStatusCode(200).withBody(returnValue.toString());
+
+        return response;
     }
 }
